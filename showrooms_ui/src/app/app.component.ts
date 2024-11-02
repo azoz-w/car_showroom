a7731c30d6676f82de50cf0f07aabc3496cd9b5e
azoz-w <azoz.m.alwaalan@gmail.com>
2024-11-02 13:44:41 +0300

    add logout functionality

--- showrooms_ui/src/app/app.component.ts
+++ showrooms_ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/components/toast/toast.component';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,16 @@ import { ToastComponent } from './shared/components/toast/toast.component';
 })
 export class AppComponent {
   title = 'showrooms_ui';
+  isLoggedIn: boolean = false;
+  constructor(private router: Router, private authSerivce: AuthService) {
+    this.authSerivce.isAuthenticated().subscribe({
+      next: (val) => {
+        this.isLoggedIn = val;
+      },
+    });
+  }
+  logout() {
+    this.authSerivce.logout();
+    this.router.navigate(['/login']);
+  }
 }
